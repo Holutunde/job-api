14b90462e0398f327d8cc34719c1b05497109857
Holutunde <Holutunde99@gmail.com>
2022-07-29 23:00:46 +0100

    setup project structure

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+require('express-async-errors')
+const connectDB = require('./database/db')
+const auth = require('./server/routes/auth')
+const jobs = require('./server/routes/jobs')
+const notFound = require('./middleware/notFound')
+const errorHandler = require('./middleware/errorHandler')
+
+app.use(express.json())
+
+app.use('/api/auth', auth)
+app.use('/api/jobs', jobs)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const port = process.env.PORT || 4000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}....`),
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
---
+++ server/controllers/jobs.js
@@ -0,0 +1,15 @@
+const createJob = async (req, res) => {}
+const getAllJobs = async (req, res) => {}
+const getJob = async (req, res) => {}
+
+const updateJob = async (req, res) => {}
+
+const deleteJob = async (req, res) => {}
+
+module.exports = {
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+}
