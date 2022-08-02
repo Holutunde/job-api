0b04a26761c6482cff34cedf599a472b04b2675d
Holutunde <Holutunde99@gmail.com>
2022-08-02 23:53:30 +0100

    get a paticular job

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -6,8 +6,30 @@ const createJob = async (req, res) => {
   const newJob = await Job.create(req.body)
   res.status(StatusCodes.CREATED).json({ newJob })
 }
-const getAllJobs = async (req, res) => {}
-const getJob = async (req, res) => {}
+
+const getAllJobs = async (req, res) => {
+  const allJobs = await Job.find({ createdBy: req.user.userId }).sort(
+    'createdAt',
+  )
+  res.status(StatusCodes.OK).json({ allJobs, count: jobs.length })
+}
+
+const getJob = async (req, res) => {
+  //   const {
+  //     user: { userId },
+  //     params: { id: jobId },
+  //   } = req
+  const { userId } = req.user
+  const { jobId } = req.params.id
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
+}
 
 const updateJob = async (req, res) => {}
 
