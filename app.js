ba7f0e5479f1be896398500b15ad669801c09cc7
Holutunde <Holutunde99@gmail.com>
2022-09-05 17:26:16 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const app = express()
 require('dotenv').config()
 require('express-async-errors')
+
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+
+const express = require('express')
+const app = express()
 const connectDB = require('./database/db')
+
+//routes
 const auth = require('./server/routes/auth')
 const jobs = require('./server/routes/jobs')
+
+//errors
 const notFound = require('./middleware/notFound')
 const errorHandler = require('./middleware/errorHandler')
+
+//authentication
 const aunthenticateUser = require('./middleware/authentication')
 
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.use('/api/auth', auth)
 app.use('/api/jobs', aunthenticateUser, jobs)
